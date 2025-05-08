95d9e0e946809bf5cda34e882b6ff26d2ff27da3
Plikii <marioua672@gmail.com>
2025-05-08 19:15:36 +0300

    lint

--- src/components/dashboard/body/todoGrid.tsx
+++ src/components/dashboard/body/todoGrid.tsx
@@ -1,38 +1,40 @@
-'use client'
+"use client";
 import { useTodoStore } from "@/store/store";
 import type { TodoListInterface } from "@/types/todoListInterface";
 import { useParams } from "next/navigation";
 import TodoItems from "./todoItem";
 
 export default function TodoGrid() {
-    const todoLists = useTodoStore(state => state.todoLists);
-    const { id } = useParams();
+  const todoLists = useTodoStore(state => state.todoLists);
+  const { id } = useParams();
 
-    const listId = Array.isArray(id) ? id[0] : id;
+  const listId = Array.isArray(id) ? id[0] : id;
 
-    if (!listId) {
-        return <div className="text-xl p-10 text-red-500">Todo not found</div>;
-    }
+  if (!listId) {
+    return <div className="text-xl p-10 text-red-500">Todo not found</div>;
+  }
 
-    const list: TodoListInterface | undefined = todoLists.find((todo) => todo.id === listId);
+  const list: TodoListInterface | undefined = todoLists.find(
+    todo => todo.id === listId,
+  );
 
-    if (!list) {
-        return <div className="text-xl p-10 text-red-500">Список не знайдений</div>;
-    }
+  if (!list) {
+    return <div className="text-xl p-10 text-red-500">Список не знайдений</div>;
+  }
 
-    return (
-        <div className="text-2xl p-10 text-title grid grid-cols-2 gap-10">
-            {list?.todoList.map(todo => (
-                <TodoItems
-                    key={todo.id}
-                    todoListId={listId}
-                    id={todo.id}
-                    title={todo.title}
-                    description={todo.description}
-                    completed={todo.completed}
-                    admin={list.admin}
-                />
-            ))}
-        </div>
-    );
+  return (
+    <div className="text-2xl p-10 text-title grid grid-cols-2 gap-10">
+      {list?.todoList.map(todo => (
+        <TodoItems
+          key={todo.id}
+          todoListId={listId}
+          id={todo.id}
+          title={todo.title}
+          description={todo.description}
+          completed={todo.completed}
+          admin={list.admin}
+        />
+      ))}
+    </div>
+  );
 }
